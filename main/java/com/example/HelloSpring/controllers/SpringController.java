1baed48b9cecb86b34e62c6f2515f88be9c68855
Ram Prakash Pathak <133332560+RamP1432@users.noreply.github.com>
2025-02-22 00:24:58 +0530

    [Ram Prakash Pathak] : using @PostMapping With @RequestBody UserDTO class.

--- main/java/com/example/HelloSpring/controllers/SpringController.java
+++ main/java/com/example/HelloSpring/controllers/SpringController.java
@@ -1,9 +1,7 @@
 package com.example.HelloSpring.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.HelloSpring.UserDTO;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SpringController {
@@ -20,6 +18,11 @@ public class SpringController {
     //http://localhost:8080/hello/param/Mark
     public String sayHello2(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
+    }
+    @PostMapping("/post")
+    //test with postman: http://localhost:8080/hello/post
+    public String sayHello3(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
     }
 
 }
