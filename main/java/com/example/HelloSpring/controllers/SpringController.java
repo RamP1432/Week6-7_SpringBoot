6ec0018738545534dcad3d653a4a83848b3347c4
Ram Prakash Pathak <133332560+RamP1432@users.noreply.github.com>
2025-02-21 23:47:10 +0530

    [Ram Prakash Pathak] using @GetMapping with @RequestParam.

--- main/java/com/example/HelloSpring/controllers/SpringController.java
+++ main/java/com/example/HelloSpring/controllers/SpringController.java
@@ -1,6 +1,7 @@
 package com.example.HelloSpring.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +10,9 @@ public class SpringController {
     public String hello(){
         return "Hello from BridgeLabz";
     }
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
 }
